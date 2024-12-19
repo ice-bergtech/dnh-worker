7bc49004c493e506a382770a2342ac189c2651ad
dev <dev@icebergtech.xyz>
2024-12-18 18:38:05 -0800

    add hono jwt auth middleware

--- dnh-d1/src/index.ts
+++ dnh-d1/src/index.ts
@@ -1,5 +1,7 @@
 import { fromHono } from "chanfana";
 import { Hono } from "hono";
+import { jwt } from 'hono/jwt';
+import type { JwtVariables } from 'hono/jwt';
 import { TaskCreate } from "./endpoints/taskCreate";
 import { TaskDelete } from "./endpoints/taskDelete";
 import { TaskFetch } from "./endpoints/taskFetch";
@@ -11,19 +13,50 @@ import { PDNSFetch, PDNSFetchReverse } from "endpoints/pdnsFetch";
 import { RateFetch } from "endpoints/rateFetch";
 
 // Start a Hono app
-const app = new Hono();
+// Setup JWT token middleware
+// Specify the variable types to infer the `c.get('jwtPayload')`:
+type Variables = JwtVariables
+const app = new Hono<{ Variables: Variables }>()
+
+app.use(
+  '/auth/*',
+  jwt({
+    secret: 'it-is-very-secret',
+  })
+)
+
+app.get('/auth/page', (c) => {
+  return c.text('You are authorized')
+})
 
 // Setup OpenAPI registry
 const openapi = fromHono(app, {
 	docs_url: "/",
+	// https://chanfana.pages.dev/user-guide/security/?h=auth
+	schema: {
+		security: [
+			{
+				BearerAuth: [],
+			}
+		]
+	}
 });
 
 // Register OpenAPI endpoints
-openapi.get("/api/tasks", TaskList);
-openapi.post("/api/tasks", TaskCreate);
-openapi.get("/api/tasks/:taskSlug", TaskFetch);
-openapi.delete("/api/tasks/:taskSlug", TaskDelete);
+// openapi.get("/api/tasks", TaskList);
+// openapi.post("/api/tasks", TaskCreate);
+// openapi.get("/api/tasks/:taskSlug", TaskFetch);
+// openapi.delete("/api/tasks/:taskSlug", TaskDelete);
 
+// https://chanfana.pages.dev/user-guide/security/?h=auth
+openapi.registry.registerComponent(
+	'securitySchemes',
+	'BearerAuth',
+	{
+		type: 'http',
+		scheme: 'bearer'
+	}
+)
 
 openapi.get("/api/rate", RateFetch);
 openapi.get("/api/domain/:domainName", DomainFetch);
